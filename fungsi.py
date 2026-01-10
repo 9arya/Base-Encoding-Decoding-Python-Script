87366e5adf200cd4c4be5dc4487d2f6f7a7c5721
9arya <aspanpinter@gmail.com>
2026-01-10 12:47:34 +0700

    Add files via upload

---
+++ fungsi.py
@@ -0,0 +1,48 @@
+from base64 import b32encode, b32decode, b64decode, b64encode, b16decode, b16encode, b85encode, b85decode
+from termcolor import colored
+from time import sleep
+import binascii
+def fungsi(tipe):
+  metodeen ={
+    85: b85encode,
+    64: b64encode,
+    32: b32encode,
+    16: b16encode
+  }
+  metodede = {
+    85: b85decode,
+    64: b64decode,
+    32: b32decode,
+    16: b16decode
+  }
+  if tipe not in metodede:
+    return
+  while True:
+    print("pilih program:\n1.encode\n2.decode\n3.kembali")
+    try:
+      pilih = int(input(">>").strip())
+      if pilih == 1:
+        pesan = input("ketik pesannya:").encode("UTF-8")
+        print("hasilnya:\n")
+        print(colored(metodeen[tipe](pesan).decode("UTF-8"), "green"))
+        sleep(0.7)
+      elif pilih == 2:
+        kode = input("apa kodenya:").encode("UTF-8")
+        print("hasilnya:\n")
+        print(colored(metodede[tipe](kode).decode("UTF-8"), "blue"))
+        print("\n")
+        sleep(0.7)
+      elif pilih == 3:
+        print("menghentikan program")
+        sleep(10)
+        break 
+      else:
+        print(colored("masukanmu salah", "red"))
+    except ValueError:
+      print(colored("masukan tidak benar", "red"))
+    except binascii.Error:
+      print("yah eror")
+    except KeyboardInterrupt:
+      print(colored("kalau mau berhenti pilih opsi ke 3 jangan pakai ctrl + c", "yellow"))
+    except EOFError:
+      print(colored("sebaiknya pakai opsi nomor 3 kalau mau pergi", "yellow"))
---
+++ main.py
@@ -0,0 +1,36 @@
+from fungsi import fungsi
+from time import sleep
+from termcolor import colored
+
+while True:
+  print("mau apa:\n1.base16\n2.base32\n3.base64\n4.base85\n5.keluar")
+  try:
+    pilihan = int(input(">>").strip())
+    if pilihan == 1:
+      sleep(0.02)
+      fungsi(16)
+    elif pilihan == 2:
+      sleep(0.02)
+      fungsi(32)
+    elif pilihan == 3:
+      sleep(0.02)
+      fungsi(64)
+    elif pilihan == 4:
+      sleep(0.02)
+      fungsi(85)
+    elif pilihan == 5:
+      sleep(0.001)
+      print("membereskan sisa program")
+      sleep(1.5)
+      print("sebentar lagi")
+      sleep(3)
+      print("selesai")
+      break
+    else:
+      print(colored("pilihanmu tidak ada dalam daftar", "yellow"))
+  except ValueError:
+    print(colored("kamu memasukan hal yang tidak sesuai", "yellow"))
+  except KeyboardInterrupt:
+    print(colored("kalau keluar pakai opsi nomor 5", "red"))
+  except EOFError:
+    print(colored("\ngak boleh pakai ctrl + D ya", "red"))
