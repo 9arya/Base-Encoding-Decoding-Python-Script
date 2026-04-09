8562793aff3fca57d077f3d858f940acbface011
9arya <aspanpinter@gmail.com>
2026-04-10 05:10:42 +0700

    Add files via upload

--- cli.py
+++ cli.py
@@ -2,58 +2,38 @@
 import sys
 from base64 import *
 import binascii
+print("\n")
+a = " ".join(sys.argv[3:]).encode()
 try:
     if len(sys.argv) < 4:
-        print("usage:\npython3 cli.py en b16 hello word")
+        print("usage:\nbed en b16 hello word")
     else:
         mtden = {
-                16: b16encode,
-                32: b32encode,
-                64: b64encode,
-                85: b85encode
+                "b16": b16encode,
+                "b32": b32encode,
+                "b32hex": b32hexencode,
+                "b64": b64encode,
+                "b85": b85encode,
+                "z85": z85encode
                 }
         mtddec = {
-                16: b16decode,
-                32: b32decode,
-                64: b64decode,
-                85: b85decode
+                "b16": b16decode,
+                "b32": b32decode,
+                "b32hex": b32hexdecode,
+                "b64": b64decode,
+                "b85": b85decode,
+                "z85": z85decode
                 }
         if sys.argv[1] == "en":
-            if sys.argv[2] == "b16":
-                a = " ".join(sys.argv[3:]).encode()
-                b = mtden[16](a)
-                print(b.decode())
-            elif sys.argv[2] == "b32":
-                a = " ".join(sys.argv[3:]).encode()
-                b = mtden[32](a)
-                print(b.decode())
-            elif sys.argv[2] == "b64":
-                a = " ".join(sys.argv[3:]).encode()
-                b = mtden[64](a)
-                print(b.decode())
-            elif sys.argv[2] == "b85":
-                a = " ".join(sys.argv[3:]).encode()
-                b = mtden[85](a)
+            if sys.argv[2] in mtden:
+                b = mtden[sys.argv[2]](a)
                 print(b.decode())
             else:
-                print("the algorithm is not support or not correct")
+                print("the algorithm not support or incorrect")
         elif sys.argv[1] == "dec":
-            if sys.argv[2] == "b16":
-                c = " ".join(sys.argv[3:]).encode()
-                d = mtddec[16](c)
-                print(d.decode())
-            elif sys.argv[2] == "b32":
-                c = " ".join(sys.argv[3:]).encode()
-                d = mtddec[32](c)
-                print(d.decode())
-            elif sys.argv[2] == "b64":
-                c = " ".join(sys.argv[3:]).encode()
-                d = mtddec[64](c)
-                print(d.decode())
-            elif sys.argv[2] == "b85":
-                c = " ".join(sys.argv[3:]).encode()
-                d = mtddec[85](c)
-                print(d.decode())
+            if sys.argv[2] in mtddec:
+                c = mtddec[sys.argv[2]](a)
+                print(c.decode())
             else:
                 print("the algorithm is not support or not correct")
 except SyntaxError:
@@ -62,3 +42,5 @@ except binascii.Error:
     print("your encode message is not same with the algorithm your pick")
 except UnicodeDecodeError:
     print("are you use not encoded text in decode options?")
+finally:
+    print("\n")
