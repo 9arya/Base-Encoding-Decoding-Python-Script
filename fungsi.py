0d63bd65f17e9881165189d879785c2920f575a6
9arya <aspanpinter@gmail.com>
2026-05-01 13:16:47 +0700

    Add files via upload

--- fungsi.py
+++ fungsi.py
@@ -1,6 +1,10 @@
-from base64 import b32encode, b32decode, b64decode, b64encode, b16decode, b16encode, b85encode, b85decode
-from termcolor import colored
 import binascii
+from base64 import (b16decode, b16encode, b32decode, b32encode, b64decode,
+                    b64encode, b85decode, b85encode)
+
+from termcolor import colored
+
+
 def fungsi(tipe):
   metodeen ={
     85: b85encode,
@@ -17,28 +21,27 @@ def fungsi(tipe):
   if tipe not in metodede:
     return
   while True:
-    print("pilih program:\n1.encode\n2.decode\n3.kembali")
+    print("pick program:\n1.encode\n2.decode\n3.back")
     try:
       pilih = int(input(">>").strip())
       if pilih == 1:
-        pesan = input("ketik pesannya:").encode("UTF-8")
-        print("hasilnya:\n")
+        pesan = input("write your message:").encode("UTF-8")
+        print("you got this:\n")
         print(colored(metodeen[tipe](pesan).decode("UTF-8"), "green"))
       elif pilih == 2:
-        kode = input("apa kodenya:").encode("UTF-8")
-        print("hasilnya:\n")
+        kode = input("write the code:").encode("UTF-8")
+        print("you got this:\n")
         print(colored(metodede[tipe](kode).decode("UTF-8"), "blue"))
         print("\n")
       elif pilih == 3:
-        print("menghentikan program")
         break 
       else:
-        print(colored("masukanmu salah", "red"))
+        print(colored("no one in the options", "red"))
     except ValueError:
-      print(colored("masukan tidak benar", "red"))
+      print(colored("the input is not valid", "red"))
     except binascii.Error:
-      print("yah eror")
+      print("wow you got an error,report it to developer")
     except KeyboardInterrupt:
-      print(colored("kalau mau berhenti pilih opsi ke 3 jangan pakai ctrl + c", "yellow"))
+        return
     except EOFError:
-      print(colored("sebaiknya pakai opsi nomor 3 kalau mau pergi", "yellow"))
+        return
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fungsi import fungsi
 from termcolor import colored
 
 while True:
-  print("mau apa:\n1.base16\n2.base32\n3.base64\n4.base85\n5.keluar")
+  print("pick algorithm:\n1.base16\n2.base32\n3.base64\n4.base85\n5.keluar")
   try:
     pilihan = int(input(">>").strip())
     if pilihan == 1:
@@ -16,10 +16,10 @@ while True:
     elif pilihan == 5:
       break
     else:
-      print(colored("pilihanmu tidak ada dalam daftar", "yellow"))
+      print(colored("no one in options", "yellow"))
   except ValueError:
-    print(colored("kamu memasukan hal yang tidak sesuai", "yellow"))
+    print(colored("your input not valid", "yellow"))
   except KeyboardInterrupt:
-    print(colored("kalau keluar pakai opsi nomor 5", "red"))
+    break
   except EOFError:
-    print(colored("\ngak boleh pakai ctrl + D ya", "red"))
+    break
